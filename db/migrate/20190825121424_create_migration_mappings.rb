0d211b2e9d19e12808cc26460eabb6a92d042383
mridulnagpal <mridulnagpal07@gmail.com>
2019-08-25 17:55:39 +0530

    Migration Mapping model added

---
+++ db/migrate/20190825121424_create_migration_mappings.rb
@@ -0,0 +1,11 @@
+class CreateMigrationMappings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :migration_mappings do |t|
+      t.string :source_type
+      t.integer :source_id
+      t.integer :crowdai_participant_id
+
+      t.timestamps
+    end
+  end
+end
