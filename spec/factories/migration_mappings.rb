a8a3fbaa321666eaad2e9681afc56c8aa5339e56
mridulnagpal <mridulnagpal07@gmail.com>
2019-08-26 15:25:15 +0530

    Factory association added

--- spec/factories/migration_mappings.rb
+++ spec/factories/migration_mappings.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :migration_mapping do
+    association :source, factory: :submission
     source_type { Submission }
     source_id { 1 }
     crowdai_participant_id { 1 }
